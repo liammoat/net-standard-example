4ae022a8cfbd4ed5929221f770e7e35b16d3a045
Liam Moat <liam.moat@microsoft.com>
2019-05-20 12:19:25 +0100

    feat(cli): .net framework console application

---
+++ src/NetStandard.Example.CLI/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetStandard.Example.Core;
+using System;
+
+namespace NetStandard.Example.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var discountEngine = new DiscountEngine();
+
+            while (true)
+            {
+                Console.WriteLine("Enter date joined:");
+                var input = Console.ReadLine();
+
+                DateTime dateJoined;
+                if (!DateTime.TryParse(input, out dateJoined))
+                {
+                    Console.WriteLine($"Invalid input: '{input}'.");
+                }
+
+                var discount = discountEngine.GetDiscountAmount(dateJoined);
+                Console.WriteLine($"Available discount: {discount * 100}%");
+            }
+        }
+    }
+}
