d7f0479bfb187c3ec036043bf7bf497bef1c07c7
Liam Moat <liam.moat@microsoft.com>
2019-05-20 12:14:45 +0100

    feat(api): .net standard class library with .net core web API

---
+++ src/NetStandard.Example.API/Controllers/DiscountsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NetStandard.Example.Core;
+
+namespace NetStandard.Example.API.Controllers
+{
+    [Route("api/discounts")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly DiscountEngine _discountEngine;
+
+        public DiscountsController(DiscountEngine discountEngine)
+        {
+            _discountEngine = discountEngine;
+        }
+
+        [HttpGet]
+        public ActionResult<double> Get(DateTime dateJoined)
+        {
+            return _discountEngine.GetDiscountAmount(dateJoined);
+        }
+    }
+}
---
+++ src/NetStandard.Example.Core/DiscountEngine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NetStandard.Example.Core
+{
+    public class DiscountEngine
+    {
+        public double GetDiscountAmount(DateTime dateJoined)
+        {
+            var diff = DateTime.Today - dateJoined;
+
+            switch ((int)Math.Floor(diff.TotalDays / 365))
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 0.1;
+                case 2:
+                case 3:
+                case 4:
+                    return 0.15;
+                case 5:
+                    return 0.2;
+                default:
+                    return 0.3;
+            }
+        }
+    }
+}
